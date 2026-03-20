725efb130fe25a9d235c4df0f91690f6050c0fcc
User1012007 <rojasbadilloe@gmail.com>
2026-03-20 10:50:09 -0600

    vehiculo.java: father class with base methods

---
+++ 3.2/vehiculo.java
@@ -0,0 +1,10 @@
+public class vehiculo {
+
+  public String marca;
+  public String patente;
+
+  public String getMarca() { return marca; }
+  public void setMarca(String marca) { this.marca = marca; }
+  public String getPatente() { return patente; }
+  public void setPatente(String patente) { this.patente = patente; }
+}
