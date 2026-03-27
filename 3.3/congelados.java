4e1a8f651f670ec59f3963529181d90de3eeef73
User1012007 <rojasbadilloe@gmail.com>
2026-03-27 13:46:41 -0600

    congelados.java: subclass

---
+++ 3.3/congelados.java
@@ -0,0 +1,11 @@
+public class congelados extends productos {
+  float temperaturaCongelacion = 0;
+
+  public congelados() { super(); }
+
+  public float getTemperaturaCongelacion() { return temperaturaCongelacion; }
+
+  public void setTemperaturaCongelacion(float temperaturaCongelacion) {
+    this.temperaturaCongelacion = temperaturaCongelacion;
+  }
+}
