c2696c9b438d0949697f51294aa8996f4e4c6625
User1012007 <rojasbadilloe@gmail.com>
2026-03-27 13:47:12 -0600

    refrigerados.java: subclass of productos

---
+++ 3.3/refrigerados.java
@@ -0,0 +1,11 @@
+public class refrigerados extends productos {
+  int codigoOrganismo = 0;
+
+  public refrigerados() { super(); }
+
+  public int getCodigoOrganismo() { return codigoOrganismo; }
+
+  public void setCodigoOrganismo(int codigoOrganismo) {
+    this.codigoOrganismo = codigoOrganismo;
+  }
+}
