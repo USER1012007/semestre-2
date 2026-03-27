5180a136c7a4ee44de7c38125a2a64195650e7d3
User1012007 <rojasbadilloe@gmail.com>
2026-03-27 13:46:58 -0600

    productos.java: superclass

---
+++ 3.3/productos.java
@@ -0,0 +1,9 @@
+public class productos {
+  String fecha = "";
+  int lote = 0;
+
+  public String getFecha() { return fecha; }
+  public void setFecha(String fecha) { this.fecha = fecha; }
+  public int getLote() { return lote; }
+  public void setLote(int lote) { this.lote = lote; }
+}
