72ee35b5256950a75d58affa62ec9664b877cdaa
User1012007 <rojasbadilloe@gmail.com>
2026-04-21 20:09:30 -0600

    Entrenador.java: class

---
+++ 4.5/Entrenador.java
@@ -0,0 +1,13 @@
+public class Entrenador {
+
+  int idFederacion;
+
+  public Entrenador(int idFederacion) { this.idFederacion = idFederacion; }
+  public void planificarEntrenamiento() {}
+
+  public int getIdFederacion() { return idFederacion; }
+
+  public void setIdFederacion(int idFederacion) {
+    this.idFederacion = idFederacion;
+  }
+}
