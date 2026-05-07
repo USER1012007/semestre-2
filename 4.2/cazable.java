f256e08dea6fd080979f9cfbd52635e849262732
User1012007 <rojasbadilloe@gmail.com>
2026-05-07 09:51:35 -0600

    cazable.java: interface for caza

---
+++ 4.2/cazable.java
@@ -0,0 +1,8 @@
+public interface cazable {
+
+  int meses_en_que_caza(int meses);
+  String tipo_de_arma();
+  String animales_cazables();
+  int cant_especimenes_cazables();
+  String nombre_parque();
+}
