c67a81718aebc0af4e0ac0dd9388f773e8e5c9d5
User1012007 <rojasbadilloe@gmail.com>
2026-05-07 09:51:55 -0600

    acuaticas.java: subclass of protegido

---
+++ 4.2/acuaticas.java
@@ -0,0 +1,19 @@
+public class acuaticas extends protegido {
+  int num_lagos;
+  double extension_lagos;
+  String nombre;
+  String especie_protegida;
+
+  public int getNum_lagos() { return num_lagos; }
+  public void setNum_lagos(int num_lagos) { this.num_lagos = num_lagos; }
+  public double getExtension_lagos() { return extension_lagos; }
+  public void setExtension_lagos(double extension_lagos) {
+    this.extension_lagos = extension_lagos;
+  }
+  public String getNombre() { return nombre; }
+  public void setNombre(String nombre) { this.nombre = nombre; }
+  public String getEspecie_protegida() { return especie_protegida; }
+  public void setEspecie_protegida(String especie_protegida) {
+    this.especie_protegida = especie_protegida;
+  }
+}
