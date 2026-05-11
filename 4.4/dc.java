e9642bc9b66876cdb7dbd37352f92039adc80c91
User1012007 <rojasbadilloe@gmail.com>
2026-05-10 19:31:34 -0600

    dc.java: subclass of super

---
+++ 4.4/dc.java
@@ -0,0 +1,14 @@
+public class dc extends Super {
+
+  int nivel;
+  public dc(String nombre, String identidad, String es_extraterreste,
+            String tipo_poder, String equipo, int nivel) {
+    super(nombre, identidad, es_extraterreste, tipo_poder, equipo);
+    this.nivel = nivel;
+  }
+
+  @Override
+  public void Pelear(int pelear, String tipo) {
+    System.out.println("nivel pelea: " + pelear + " en " + tipo);
+  }
+}
