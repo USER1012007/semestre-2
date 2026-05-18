db188a55aaa0f3a793e1a7c0d667e6809cd7a3f8
User1012007 <rojasbadilloe@gmail.com>
2026-05-18 10:05:05 -0600

    try 2

---
+++ 5.1/try2.java
@@ -0,0 +1,10 @@
+public class try2 {
+  public static void main(String arg[]) {
+    int[] array = new int[20];
+    try {
+      array[-3] = 24;
+    } catch (ArrayIndexOutOfBoundsException excepcion) {
+      System.out.println(" Error de índice en un array");
+    }
+  }
+}
