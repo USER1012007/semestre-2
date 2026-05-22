38d8e91b097d793ff060e7d0af708fbf63a222f4
User1012007 <rojasbadilloe@gmail.com>
2026-05-22 13:31:30 -0600

    idk

---
+++ 5.4/GrabArray.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class GrabArray {
+  public static void main(String[] args) throws IOException {
+    FileOutputStream fw = new FileOutputStream("datos.dat", false);
+    DataOutputStream ds = new DataOutputStream(fw);
+    int[] m = {5, 10, 3, 6};
+    for (int i = 0; i < m.length; i++) {
+      ds.writeInt(m[i]);
+    }
+    ds.close();
+  }
+}
